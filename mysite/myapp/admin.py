4e38f418dec5c4b2dfd626c0b025ed8c3b5aa2b0
glendasp <glendasp@gmail.com>
2016-12-06 23:32:51 -0600

    start admin's set up

---
+++ mysite/myapp/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .forms import SignUpForm
+from .models import SignUp
+
+# Register your models here.
+class SignUpAdmin(admin.ModelAdmin):
+    # Customizing how adm works: displaying what I want to see
+    # list_display = ["__str__", "timestamp", "updated"]
+    # form = SignUpForm
+    class Meta:
+        list_display = ["__str__", "timestamp", "updated"]
+        model = SignUp
+
+
+admin.site.register(SignUp, SignUpAdmin)
+
+
+''' Reference:
+https://docs.djangoproject.com/en/1.8/ref/contrib/admin/
+'''
+
+
+
+
