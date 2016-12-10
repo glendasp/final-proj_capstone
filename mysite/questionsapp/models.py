0ca52637b0d08e2338baa2f232beebd88b2f3dac
glendasp <glendasp@gmail.com>
2016-12-10 14:59:56 -0600

    rename

--- mysite/questionsapp/models.py
+++ mysite/questionsapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-# Model for the questions
+# Model for the questionsapp
 class Question(models.Model):
     question = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True, auto_now=False)
