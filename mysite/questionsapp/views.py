10c7646beb7e4def311e3db3f2b532e5b4e7cd19
glendasp <glendasp@gmail.com>
2016-12-11 13:17:51 -0600

    set up views for question model

--- mysite/questionsapp/views.py
+++ mysite/questionsapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from mysite.questionsapp.models import Question
+
+
+def home(request):
+    if request.user.is_authenticated():
+        queryset = Question.objects.all().order_by('-timestamp')
+        context = {
+            "queryset": queryset
+        }
+        return render(request, "questions/home.html", context)
+    else:
+        pass
+
